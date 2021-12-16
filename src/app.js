e7d5d1e5abf9b1b37e2fac7e0454719d7aa3a0c2
Keenan <kkan68@gmail.com>
2021-12-16 00:18:18 +0000

    experiment

--- src/app.js
+++ src/app.js
@@ -2,10 +2,39 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let board = [null, null, null, null, null, null, null, null, null];
+let currentPlayer = "times";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = render;
+//write your code here
+function render() {
+  document.querySelector(".board").innerHTML = board
+    .map(elm => drawGrid(elm))
+    .join("");
+
+  document.querySelectorAll(".grid").forEach((node, index) =>
+    node.addEventListener("click", function() {
+      play(index);
+    })
+  );
+}
+
+//action function
+function play(index) {
+  board[index] = currentPlayer;
+
+  if (currentPlayer === "times") {
+    currentPlayer = "circle";
+  } else {
+    currentPlayer = "times";
+  }
+
+  render();
+}
+
+//rendering function
+function drawGrid(symbol = "times") {
+  return `<div class="grid">
+  <i class="fas fa-${symbol}"></i>
+</div>`;
+}
